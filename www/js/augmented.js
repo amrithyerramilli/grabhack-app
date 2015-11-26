f4c97c29f7c2fd2c3a2b2d0cd81109d2169a9f7a
Amrith Yerramilli <amrith.yerramilli@gmail.com>
2015-11-26 15:52:20 +0530

    added augmented reality code and the 'worlds'

---
+++ www/js/augmented.js
@@ -0,0 +1,64 @@
+angular.module('grabhack-ar', [])
+    .factory('WikitudeFactory', function($q, $ionicPlatform) {
+        var worlds = [{
+            "path": "www/world/scene1/index.html",
+            "requiredFeatures": [
+                "2d_tracking"
+            ],
+            "startupConfiguration": {
+                "camera_position": "back"
+            }
+        }, {
+            "path": "www/world/scene2/index.html",
+            "requiredFeatures": [
+                "2d_tracking"
+            ],
+            "startupConfiguration": {
+                "camera_position": "back"
+            }
+        }, {
+            "path": "www/world/scene3/index.html",
+            "requiredFeatures": [
+                "2d_tracking"
+            ],
+            "startupConfiguration": {
+                "camera_position": "back"
+            }
+        }, {
+            "path": "www/world/scene4/index.html",
+            "requiredFeatures": [
+                "2d_tracking"
+            ],
+            "startupConfiguration": {
+                "camera_position": "back"
+            }
+        }];
+
+        return {
+            isDeviceSupported: true,
+            callARView: function(worldIndex) {
+                $ionicPlatform.ready(function() {
+                    var world = worlds[worldIndex];
+                    app.wikitudePlugin = cordova.require("com.wikitude.phonegap.WikitudePlugin.WikitudePlugin");
+                    app.wikitudePlugin.isDeviceSupported(function() {
+
+                        app.wikitudePlugin.loadARchitectWorld(function(loadedURL) {
+                            console.log('load arch success')
+                        }, function errorFn(error) {
+                            console.log('Loading AR web view failed: ' + error);
+                        }, world.path, world.requiredFeatures, world.startupConfiguration);
+
+
+                        //Callback method
+                        app.wikitudePlugin.setOnUrlInvokeCallback(function(url) {
+                            console.log('url invoked')
+                            $q.defer().resolve(url);
+                            app.wikitudePlugin.close();
+                        });
+                    }, function errorFn(error) {
+                        console.log('Loading AR web view failed: ' + error);
+                    }, world.requiredFeatures);
+                });
+            }
+        }
+    })
